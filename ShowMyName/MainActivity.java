a6c6c4e3096f0f47b3599110a96400f18fce603c
Teo Espero <38012835+teoespero@users.noreply.github.com>
2023-07-05 20:11:50 -0700

    Add files via upload

---
+++ ShowMyName/MainActivity.java
@@ -0,0 +1,44 @@
+package teoespero.showmyname;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * MainActivity defines the logic that shows the information entered by the user.
+ * @author Teo Espero (BS Software Development, WGU)
+ * @version 1.0.07052023
+ * @since 07/05/2023
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnShowMyName;
+    private TextView txtName;
+    private EditText editTextName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowMyName = findViewById(R.id.btnShowMyName);
+        txtName = findViewById(R.id.txtName);
+        editTextName = findViewById(R.id.editTextName);
+
+        btnShowMyName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String theName = editTextName.getText().toString();
+                if (!theName.isEmpty()){
+                    txtName.setText("Hello " + theName + "! Nice meeting you.");
+                }else{
+                    txtName.setText("Hello there, what's your name?");
+                }
+            }
+        });
+    }
+}
